0a7375c4ad8241e6ca117c77843690de0ca06769
Dheeraj Komandur <dheerajkomandur@gmail.com>
2019-04-24 15:44:18 +0530

    Renamed Activities

--- app/src/main/java/in/creationdevs/railwaycomplaintapp/SplashActivity.java
+++ app/src/main/java/in/creationdevs/railwaycomplaintapp/SplashActivity.java
@@ -18,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,Main2Activity.class);
+                Intent intent=new Intent(SplashActivity.this,GetPhoneActivity.class);
                 startActivity(intent);
                 finish();
             }
